2c230eb61ec165e2d2ab02bed6a35b6752d58d0d
abudhosamuel <abudhosamuel@gmail.com>
2024-10-03 13:01:46 +0300

    remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,18 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        # Return the raw content as a byte string
+        response = requests.get(self.url)
+        return response.content  # Return the response as bytes
 
     def load_json(self):
-        pass
+        # Convert the response to JSON after getting the response body
+        response_body = self.get_response_body()
+        try:
+            return json.loads(response_body)
+        except json.JSONDecodeError:
+            return {}
